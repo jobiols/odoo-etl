c0465e3691fbd8df7b999e50eb32e95abe2f25e1
Jorge Obiols <jorge.obiols@gmail.com>
2020-11-03 16:32:00 -0300

    restaurar l10n_ar

--- l10n_ar/__manifest__.py
+++ l10n_ar/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Argentinian Accounting FOR MIG',
-    'version': "4.0",
+    'name': 'Argentinian Accounting',
+    'version': "3.0",
     'description': """
 Functional
 ----------
